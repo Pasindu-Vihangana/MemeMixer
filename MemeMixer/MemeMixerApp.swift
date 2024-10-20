0e5a32352a79f5534c464ccd75ba82acf2820e21
Pasindu Vihangana <pasi1028@gmail.com>
2024-10-20 12:13:14 +0530

    Initial Commit

---
+++ MemeMixer/MemeMixerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemeMixerApp.swift
+//  MemeMixer
+//
+//  Created by Pasindu Vihangana on 2024-10-20.
+//
+
+import SwiftUI
+
+@main
+struct MemeMixerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
